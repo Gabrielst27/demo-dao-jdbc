3258ddb3adc60b1770c15f5e42c894e22d1cf371
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-01-09 19:24:00 -0300

    FindById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(1, "Gabriel", "gabrielst328@gmail.com", new Date(), 3000.00, obj);
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 	}
 
